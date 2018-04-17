5b04b731dd8a1d0649fda5ef56212e54d84befd6
timanim <timothy_chen@live.com>
2018-04-17 14:56:35 +0800

    webpack

---
+++ src/main.js
@@ -0,0 +1,4 @@
+require('../js-filter-common/css/arrayFilter.css')
+require('../assets/font/font-awesome/css/font-awesome.css')
+var initFn = require('../js-filter-common/js/arrayFilter.js')
+initFn.init()
